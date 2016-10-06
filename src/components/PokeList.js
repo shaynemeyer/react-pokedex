25851b0f2f0408e84e8cb06c017a0b7b40924c75
Shayne Meyer <shayne.meyer@gmail.com>
2016-10-06 06:21:29 -0700

    added stuff

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 import './PokeList.css';
 import PokeListItem from './PokeListItem';
-// import {pokedex} from '../data/pokedex';
+import {pokedex} from '../data/pokedex';
 
 class PokeList extends Component {
 	render() {
-		const listItems = this.state.pokelist.map(function(item){
+		const listItems = pokedex.map(function(item){
 			return <PokeListItem key={item.pid} pid={item.pid} name={item.name} next={item.next} prev={item.prev}/>;
 		});
 
--- src/components/PokeListItem.js
+++ src/components/PokeListItem.js
@@ -1,11 +1,34 @@
 import React, {Component} from 'react';
 import './PokeListItem.css';
 
+
+const imageUrlBase = "/images/Pokemon/hd/";
+
 class PokeListItem extends Component {
 	render() {
 		return (
-			<div>
+			<div className="panel panel-default col-xs-3" style={PanelWrapperStyle}>
+				<Link to={`/pokemon/${this.props.pid}`}>
+					<div className="panel-heading">
+						<figure style={FigureStyle}>
+							<img src={`${imageUrlBase}${this.props.pid}.png`} width="75%" alt={this.props.name} style={ImageStyle} />
+						</figure>
+					</div>
+					<div className="panel-body">
 
+
+						<span style={NotchStyle}></span>
+						<div className="pokemon-info" style={PokemonInfoStyle}>
+							<p className="pokemon-id" style={PokemonIdStyle}>
+								# {this.props.pid}
+							</p>
+							<h3 style={NameStyle}>{this.props.name}</h3>
+							<div className="abilities" style={AbilitiesStyle}>
+								{abilities}
+							</div>
+						</div>
+					</div>
+				</Link>
 			</div>
 		);
 	}
